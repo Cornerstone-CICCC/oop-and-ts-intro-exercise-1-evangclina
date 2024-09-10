5842a41b74413c2c6d06e3fa4c5c63c7e68e67db
Evangelina Vega <evangelina1001@hotmail.com>
2024-09-10 16:06:37 -0700

    Finished Ex 2

--- exercise2.js
+++ exercise2.js
@@ -7,6 +7,25 @@
 
 class BankAccount {
   // YOUR CODE HERE
+  constructor(accountNumber, balance){
+    this.accountNumber = accountNumber
+    this.balance = balance
+  }
+
+  deposit(amount){
+    this.balance = this.balance + amount
+  }
+  withdraw(amount){
+    this.balance = this.balance - amount
+  }
+
+  static compareAccounts(account1, account2){
+    if (account1.balance > account2.balance){
+      return account1
+    }else{
+      return account2
+    }
+  }
 }
 
 
@@ -14,7 +33,9 @@ class BankAccount {
 const account1 = new BankAccount(123456, 500);
 const account2 = new BankAccount(654321, 1000);
 account1.deposit(300); // account1 becomes 800
+console.log(account1.balance)
 account2.withdraw(300); // account2 becomes 700
+console.log(account2.balance)
 const richerAccount = BankAccount.compareAccounts(account1, account2);
 console.log(`Account ${richerAccount.accountNumber} has the higher balance.`);
 // Expected output: "Account 123456 has the higher balance."
